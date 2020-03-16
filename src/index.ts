6abc282baf72d07e00ba321e42fc018a8c10e1c8
taoqf <tao_qf@163.com>
2020-03-16 14:00:02 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import { toDataURL } from 'qrcode';
 
-export default function an20(param1: string) {
+/**
+ * 生成二维码
+ * @param data_for_qrcode	二维码的数据
+ * @param width				二维码的宽
+ * @param dark_color		二维码的颜色
+ * @param light_color		背景色
+ */
+export default function create_qr(data_for_qrcode: string, width: number, dark_color: string, light_color: string) {
+	return toDataURL(data_for_qrcode, {
+		color: {
+			dark: dark_color,	// black pixels
+			light: light_color	// white background
+		},
+		errorCorrectionLevel: 'H',	// LMQH
+		margin: 1,
+		width
+	});
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,16 @@
 const test = require('ava');
+const { promises } = require('fs');
+
+const { readFile } = promises;
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('qrcode', async (t) => {
+	const dataForQRcode = 'https://mm-edu.gitee.io';		// 二维码的数据
+	const width = 80;				// 二维码的宽
+	const dark_color = '#000000';	// 二维码的颜色
+	const light_color = '#ffffff';	// 背景色
+	const r = await a(dataForQRcode, width, dark_color, light_color);
+	const f = await readFile('./tests/test.png');
+	t.is(r.replace(/.*,/, ''), f.toString('base64'));
 });
